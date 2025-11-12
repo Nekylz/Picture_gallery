7917bfa48ae98cf5217929d7c56173d5313bd7ce
Dev-by-robin <robinkorbee@gmail.com>
2025-11-12 11:20:56 +0100

    Add project files.

---
+++ PictureGallery/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PictureGallery
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ PictureGallery/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PictureGallery
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
